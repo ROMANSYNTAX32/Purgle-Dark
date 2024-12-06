ccbc3f8279a41365872922f353aa6a65cb0a81a4
Moonie Wentz <wentzmoonie249@gmail.com>
2024-12-06 08:32:55 -0500

    Editor foreground updated

---
+++ examples/cmake_example.cmake
@@ -0,0 +1,11 @@
+#
+# CMakeLists.txt
+# By, Moonie Wentz.
+#
+
+# Include cmake helpers
+
+include(cmake/utilities.cmake)
+
+set(JUCE_GIT_URL https://github.com/juce-framework/JUCE.git)
+set(JUCE_LIB_PATH get_relative(libraries/juce))
